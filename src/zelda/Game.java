f09a7a678e704abeb923b7d2a64fa3b5e0b39beb
Jean <franciscojean053@gmail.com>
2024-03-08 15:17:54 -0300

    Adicionando Movimentacoes

--- src/zelda/Game.java
+++ src/zelda/Game.java
@@ -4,15 +4,20 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable, KeyListener{
 	public static int Width = 480, Height = 480;
+	public Player player;
 	
 	public Game() {
+		this.addKeyListener(this);
 		this.setPreferredSize(new Dimension(Width, Height));
+		player = new Player(0, 0);
 	}
 	
 	@Override
@@ -29,7 +34,7 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public void tick() {
-		
+		player.tick();
 	}
 	
 	public void render() {
@@ -46,8 +51,7 @@ public class Game extends Canvas implements Runnable{
 		g.fillRect(0, 0, Width, Height);
 		
 		
-		g.setColor(Color.red);
-		g.fillRect(0, 0, 50, 50);
+		player.render(g);
 		
 		bs.show();
 	}
@@ -63,5 +67,38 @@ public class Game extends Canvas implements Runnable{
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setVisible(true);
 		new Thread(game).start();
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		if(arg0.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = true;
+		} else if(arg0.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = true;
+		}
+		
+		if(arg0.getKeyCode() == KeyEvent.VK_UP) {
+			player.up = true;
+		} else if(arg0.getKeyCode() == KeyEvent.VK_DOWN) {
+			player.down = true;
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		if(arg0.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = false;
+		} else if(arg0.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = false;
+		}
+		
+		if(arg0.getKeyCode() == KeyEvent.VK_UP) {
+			player.up = false;
+		} else if(arg0.getKeyCode() == KeyEvent.VK_DOWN) {
+			player.down = false;
+		}
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		
 	}
 }
